1903e604e570de99cd46406bef522f172d566ffa
srrifat10 <srrifat781@gmail.com>
2021-04-15 16:04:13 +0600

    add more 7

---
+++ 7.Aveage2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double A,B,C,med;
+    cin>>A>>B>>C;
+    med=((A*2)+(B*3)+(C*5))/10;
+    cout<<"MEDIA = "<<fixed<<setprecision(5)<<med<<endl;
+    return 0;
+}
